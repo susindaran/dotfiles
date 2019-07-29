8390587432b1874f77bf1952ac0b1e8119f3421d
Susindaran Elangovan <susindaran94@gmail.com>
2019-07-29 15:31:36 -0700

    Add bunch more defaults for emacs

--- emacs.d/settings/defaults.el
+++ emacs.d/settings/defaults.el
@@ -2,13 +2,20 @@
 (setq make-backup-files nil)
 
 ;; stop creating #autosave# files
-(setq auto-save-default nil) 
+(setq auto-save-default nil)
+(setq auto-save-list-file-prefix nil)
+
+;; stop creating lockfiles
+(setq create-lockfiles nil)
 
 ;; Show keystrokes in progress
 (setq echo-keystrokes 0.1)
 
 ;; Answering just 'y' or 'n' will do
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; To avoid accidental cmd-q kills
+(setq confirm-kill-emacs 'yes-or-no-p)
 
 ;; Remove text in active region if inserting text
 (delete-selection-mode 1)
@@ -19,27 +26,31 @@
 ;; Show me empty lines after buffer end
 (set-default 'indicate-empty-lines t)
 
+;; Show trailing whitespace
+(setq-default show-trailing-whitespace t)
+
 ;; Sentences do not need double spaces to end. Period.
 (set-default 'sentence-end-double-space nil)
+
+;; Show matching parentheses without any delay
+(setq show-paren-delay 0)
+
+;; Select help window when opened
+(setq help-window-select t)
 
 ;; Smooth scrolling
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 2) ((control) . nil)))
 (setq mouse-wheel-progressive-speed nil)
 
-;; Make ibuffer default
-(defalias 'list-buffers 'ibuffer)
+;; Set column size to 80 characters in org-mode
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-;; ido-mode settings
-(setq ido-enable-flex-matching t)
-(ido-mode 1)
+;; Uniquify buffer names using a style that matches files paths
+(setq uniquify-buffer-name-style 'forward)
 
-;; display items vertically
-(setq ido-separator "\n")
-
-;; use current pane for newly opened file
-(setq ido-default-file-method 'selected-window)
-
-;; use current pane for newly switched buffer
-(setq ido-default-buffer-method 'selected-window)
+;; Quickly close all buffers to reset emacs to a clean state
+(defun close-all-buffers ()
+  (interactive)
+  (mapc 'kill-buffer (buffer-list)))
 
 (provide 'defaults)
