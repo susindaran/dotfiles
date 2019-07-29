bc19d82ce06f15571aa043174a08b58b8f1029f9
Susindaran Elangovan <susindaran94@gmail.com>
2019-07-29 15:35:45 -0700

    Add helm packages | Add language mode packages | Add some text editing utility packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,10 @@
 ;; Set up load path
 (add-to-list 'load-path settings-dir)
 
+;; Set up exec-path
+(add-to-list 'exec-path "/usr/local/bin/")
+
+
 ;; #############################################
 ;; PACKAGES
 ;; #############################################
@@ -29,16 +33,30 @@
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
+;; Use same paths as user environment on macOS
+(use-package exec-path-from-shell
+  :if (eq system-type 'darwin)
+  :custom
+  (exec-path-from-shell-check-startup-files nil)
+  (exec-path-from-shell-variables '("PATH"
+                                    "MANPATH"
+                                    "GOPATH"))
+  :config
+  (exec-path-from-shell-initialize))
+
 ;; GUI theme
 (use-package dracula-theme)
 
-;; For project interaction
-(use-package projectile
-  :bind-keymap
-  ("M-p" . projectile-command-map)
+;; Deletes trailing whitespace from edited lines
+(use-package ws-butler
   :config
-  (projectile-mode +1)
-  (setq projectile-project-search-path '("~/projects/")))
+  (setq ws-butler-keep-whitespace-before-point nil)
+  (ws-butler-global-mode 1))
+
+;; Move to the first non-whitespace character on the line
+(use-package crux
+  :bind
+  ("C-a" . crux-move-beginning-of-line))
 
 ;; NerdTREE like project tree GUI
 (use-package neotree
@@ -73,6 +91,71 @@
   (global-set-key (kbd "s-{") 'elscreen-previous)
   (global-set-key (kbd "s-}") 'elscreen-next))
 
+;; Helm to the resuce
+(use-package helm
+  :config
+  (require 'helm-config)
+  (setq
+   helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+   helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+   helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+   helm-ff-file-name-history-use-recentf t
+   helm-echo-input-in-header-line        t
+   helm-autoresize-max-height           20
+   helm-autoresize-min-height            0
+   helm-M-x-fuzzy-match                  t
+   helm-buffers-fuzzy-matching           t
+   helm-recentf-fuzzy-match              t)
+  (helm-autoresize-mode 1)
+  (helm-mode 1))
+
+;; Make projectile even better
+(use-package helm-projectile
+  :config
+  (add-to-list 'projectile-known-projects "~/org/")
+  (projectile-global-mode)
+  (setq projectile-completion-system 'helm)
+  (setq projectile-enable-caching t)
+
+  (helm-projectile-on)
+  :after (helm)
+  :requires helm)
+
+;; Let's not forget ag
+(use-package helm-ag
+  :config
+  ;; Enable fuzzy matching
+  (setq helm-ag-fuzzy-match t)
+  ;; Use .agignore file at project root
+  (setq helm-ag-use-agignore t)
+  ;; Use ripgrep instead of ag
+  (setq helm-ag-base-command "rg-wrapper --vimgrep --no-heading --smart-case")
+  :after (helm)
+  :requires helm)
+
+;; Ruby
+(use-package ruby-mode
+  :config
+  (defun my-ruby-mode-hook ()
+    (set-fill-column 80)
+    (add-hook 'before-save-hook 'delete-trailing-whitespace nil 'local)
+    (setq ruby-insert-encoding-magic-comment nil))
+  (add-hook 'ruby-mode-hook 'my-ruby-mode-hook))
+
+;; Terraform
+(use-package terraform-mode)
+
+;; JSON
+(use-package json-mode)
+
+;; Markdown
+(use-package markdown-mode
+  :custom
+  (markdown-hide-urls t))
+
+;; YAML
+(use-package yaml-mode)
+
 ;; Defaults
 (require 'defaults)
 
@@ -94,7 +177,7 @@
  '(global-linum-mode t)
  '(package-selected-packages
    (quote
-    (nord-theme elscreen escreen ace-jump-mode ace-window appearance auto-package-update neotree dracula-theme projectile use-package))))
+    (yaml-mode markdown-mode json-mode magit terraform-mode helm-config nord-theme elscreen escreen ace-jump-mode ace-window appearance auto-package-update neotree dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
