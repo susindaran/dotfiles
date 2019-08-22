e7b66182b94a6e7e330c3d0d47c6897a83ea1196
Susindaran Elangovan <susindaran94@gmail.com>
2019-08-22 16:56:26 -0700

    Add go auto-completion company-go

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -200,6 +200,13 @@
 (use-package company
   :config (global-company-mode))
 
+;; Auto-completion for go
+(use-package company-go
+  :config
+  (add-hook 'go-mode-hook (lambda ()
+			    (set (make-local-variable 'company-backends) '(company-go))
+			    (company-mode))))
+
 ;; Jump to definition (dumb but effective)
 (use-package dumb-jump
   :config
@@ -241,7 +248,7 @@
  '(global-linum-mode t)
  '(package-selected-packages
    (quote
-    (go-mode org-bullets ws-butler lsp-mode company-lsp lsp-ui dumb-jump company-flow flycheck-flow flycheck company yaml-mode markdown-mode json-mode magit terraform-mode helm-config nord-theme elscreen escreen ace-jump-mode ace-window appearance auto-package-update neotree dracula-theme use-package))))
+    (company-go go-mode org-bullets ws-butler lsp-mode company-lsp lsp-ui dumb-jump company-flow flycheck-flow flycheck company yaml-mode markdown-mode json-mode magit terraform-mode helm-config nord-theme elscreen escreen ace-jump-mode ace-window appearance auto-package-update neotree dracula-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
