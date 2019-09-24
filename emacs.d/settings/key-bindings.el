04ebbd197f71c202f7fb365feb14ba80a17d0270
Susindaran Elangovan <susindaran94@gmail.com>
2019-09-24 11:15:26 -0700

    Use projectile-find-file instead of helm

--- emacs.d/settings/key-bindings.el
+++ emacs.d/settings/key-bindings.el
@@ -28,7 +28,7 @@
 (global-set-key (kbd "M-P") 'helm-projectile-switch-project)
 
 ;; Find file
-(global-set-key (kbd "M-O") 'helm-projectile-find-file)
+(global-set-key (kbd "M-O") 'projectile-find-file)
 
 ;; Search this file using ag
 (global-set-key (kbd "s-f") 'helm-do-ag-this-file)
